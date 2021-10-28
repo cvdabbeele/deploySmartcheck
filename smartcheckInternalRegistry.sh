c7f8999a740c8bc0d22b95feb3c77b10613990dc
cvdabbeele <cvdabbeele@gmail.com>
2021-10-28 13:34:09 +0200

    removed space from "Internal Registry"

--- smartcheckInternalRegistry.sh
+++ smartcheckInternalRegistry.sh
@@ -19,10 +19,10 @@ fi
 DSSC_BEARERTOKEN=$(curl -s -k -X POST https://${DSSC_HOST}/api/sessions -H "Content-Type: application/json"  -H "Api-Version: 2018-05-01" -H "cache-control: no-cache" -d "{\"user\":{\"userid\":\"${DSSC_USERNAME}\",\"password\":\"${DSSC_PASSWORD}\"}}" | jq '.token' | tr -d '"')
 [ ${VERBOSE} -eq 1 ] && printf "\n%s\n" "Bearer Token = ${DSSC_BEARERTOKEN} \n"
 
-# Adding internal repository to SmartCheck:
+# Adding internal registry to SmartCheck:
 # ------------------------------------------
-[ ${VERBOSE} -eq 1 ] && printf "\n%s\n" "Adding internal repository to SmartCheck"
-DSSC_REPOID=$(curl -s -k -X POST https://$DSSC_HOST/api/registries?scan=true -H "Content-Type: application/json" -H "Api-Version: 2018-05-01" -H "Authorization: Bearer $DSSC_BEARERTOKEN" -H 'cache-control: no-cache' -d "{\"name\":\"Internal Registry\",\"description\":\"added by  ChrisV\n\",\"host\":\"${DSSC_HOST}:5000\",\"credentials\":{\"username\":\"${DSSC_REGUSER}\",\"password\":\"$DSSC_REGPASSWORD\"},\"insecureSkipVerify\":"true"}" | jq '.id')
+[ ${VERBOSE} -eq 1 ] && printf "\n%s\n" "Adding internal registry to SmartCheck"
+DSSC_REPOID=$(curl -s -k -X POST https://$DSSC_HOST/api/registries?scan=true -H "Content-Type: application/json" -H "Api-Version: 2018-05-01" -H "Authorization: Bearer $DSSC_BEARERTOKEN" -H 'cache-control: no-cache' -d "{\"name\":\"Internal_Registry\",\"description\":\"added by  ChrisV\n\",\"host\":\"${DSSC_HOST}:5000\",\"credentials\":{\"username\":\"${DSSC_REGUSER}\",\"password\":\"$DSSC_REGPASSWORD\"},\"insecureSkipVerify\":"true"}" | jq '.id')
 printf "\n%s\n" "Repo added with id: ${DSSC_REPOID}"
 
-#TODO: write a test to verify if the Repository was successfully added
+#TODO: write a test to verify if the Registry was successfully added
