24d3ee3ad582a4779eb0e26916ddc477a02ba20e
cvdabbeele <cvdabbeele@gmail.com>
2021-11-04 21:00:01 +0100

    removed spaces in k8s   .crt

--- smartcheck.sh
+++ smartcheck.sh
@@ -62,10 +62,10 @@ EOF
   fi
   
   printf '%s' "Creating certificate for loadballancer...  "
-  openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout ${WORKDIR}/k8s.key -out ${WORKDIR}/k8s.  crt -subj "/CN=${DSSC_SUBJECTALTNAME}" -extensions san -config ${WORKDIR}/req.conf
+  openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout ${WORKDIR}/k8s.key -out ${WORKDIR}/k8s.crt -subj "/CN=${DSSC_SUBJECTALTNAME}" -extensions san -config ${WORKDIR}/req.conf
   
   printf '%s' "Creating secret with keys in Kubernetes...  "
-  kubectl create secret tls k8s-certificate --cert=${WORKDIR}/k8s.crt --key=${WORKDIR}/k8s.key --dry-run=client   -n ${DSSC_NAMESPACE} -o yaml | kubectl apply -f -
+  kubectl create secret tls k8s-certificate --cert=${WORKDIR}/k8s.crt --key=${WORKDIR}/k8s.key --dry-run=client -n ${DSSC_NAMESPACE} -o yaml | kubectl apply -f -
   
   
   # Create overrides.yml
